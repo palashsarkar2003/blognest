eb843fb42103519925650c52574e56ab2d313503
palashsarkar07 <psarkar5683@gmail.com>
2025-09-06 23:33:17 +0530

    All updated

--- backend/index.js
+++ backend/index.js
@@ -38,7 +38,7 @@ try {
 //Cors
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,1 +1,1 @@
-export const BACKEND_URL = "https://blognest-vjoz.onrender.com"
+export const BACKEND_URL = "https://blognest-vjoz.onrender.com" 
