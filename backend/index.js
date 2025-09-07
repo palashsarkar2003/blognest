3399395c24c95e85d19aaa9f3868955dee9db779
palashsarkar07 <psarkar5683@gmail.com>
2025-09-07 21:18:58 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -18,14 +18,7 @@ dotenv.config({
 
 //Cors
 app.use(
-  cors({
-    origin: process.env.FRONTEND_URL || "https://blognest-omega.vercel.app",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Origin", "Content-Type", "X-Auth-Token"],
-    preflightContinue: false, // let CORS respond automatically
-    optionsSuccessStatus: 200 // for legacy browsers
-  })
+  cors()
 );
 
    
