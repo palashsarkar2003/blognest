9f4c56657d48e34628b8f9097685835e0909e31f
palashsarkar07 <psarkar5683@gmail.com>
2025-09-07 16:39:46 +0530

    OPTIONS ADDED

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(
   cors({
     origin:process.env.FRONTEND_URL|| 'https://blognest-omega.vercel.app',
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"]
+    methods: ["GET", "POST", "PUT", "DELETE","OPTIONS"]
   })
 );
 
